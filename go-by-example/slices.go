1963101507a2edd83430b04e45b19586da4b9164
biko <steven.biko@savannahinformatics.com>
2024-03-15 16:08:47 +0300

    chore: Golang maps data structures with slices refactoring

--- go-by-example/slices.go
+++ go-by-example/slices.go
@@ -1,6 +1,6 @@
 package main
 
-importy (
+import (
 	"fmt"
 	"slices"
 )
@@ -8,10 +8,10 @@ importy (
 func main() {
 
 	var s []string
-	fmt.Println("uninit:", s, s == nil, len(s) == 0)
+    fmt.Println("uninit:", s, s == nil, len(s) == 0)
 
 	s = make([]string, 3)
-	fmt.Println("emp:", s, "len:" len(s), "cap:", cap(s))
+    fmt.Println("emp:", s, "len:", len(s), "cap:", cap(s))
 
 
 	s[0] = "a"
@@ -40,18 +40,20 @@ func main() {
 	fmt.Println("sl3:", l)
 
 	t := []string{"g", "h", "i"}
+	fmt.Println("dcl:", t)
+
+	t2 := []string{"g", "h", "i"}
 	if slices.Equal(t, t2){
-		fmt.Println("t === t2")
+		fmt.Println("t == t2")
 	}
 
 	twoD := make([][]int, 3)
-
-	for i := 0; i < 3; i++ {
-		innerLen := i + 1
-		twoD[i] = make([]int, innerLen)
-		for j := 0; < innerLen; j++ {
-			twoD[i][j] = i + j
-		}
-	}
+    for i := 0; i < 3; i++ {
+        innerLen := i + 1
+        twoD[i] = make([]int, innerLen)
+        for j := 0; j < innerLen; j++ {
+            twoD[i][j] = i + j
+        }
+    }
 	fmt.Println("2d: ", twoD)
 }
