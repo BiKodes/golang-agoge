57183d5ea966e8244a837887c1eff9cc101358d3
biko <steven.biko@savannahinformatics.com>
2024-03-23 11:49:37 +0300

    feat: Gorrila mux router

---
+++ go-web-examples/gorrila_max.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"github.com/gorilla/mux"
+)
+
+
+func main() {
+	r := mux.NewRouter()
+
+	r.HandleFunc("/books/{title}/page/{page}", func(w http.ResponseWriter, r *http.Request){
+
+		vars := mux.vars(r)
+		title := vars["title"]
+		page := vars["page"]
+
+		fmt.Fprint(w, "You've requested the  book: %s on page %s\n", title, page)
+	})
+
+	// Restrict the request handler to specific HTTP methods.
+	r.HandleFunc("/books/{title}", CreateBook).Methods("POST")
+	r.HandleFunc("/books/{title}", ReadBook).Methods("GET")
+	r.HandleFunc("/books/{title}", UpdateBook).Methods("PUT")
+	r.HandleFunc("/books/{title}", DeleteBook).Methods("DELETE")
+
+	// Hostnames & Subdomains
+	r.HandleFunc("/books/{title}", BookHandler).Host("www.mybookstore.com")
+
+	// Schemes
+	r.HandleFunc("/secure", SecureHandler).Schemes("https")
+	r.HandleFunc("/insecure", InsecureHandler).Schemes("http")
+
+	// Path Prefixes & Subrouters
+	bookrouter := r.PathPrefix("/books").Subrouter()
+	bookrouter.HandleFunc("/", AllBooks)
+	bookrouter.HandleFunc("/{title}", GetBook)
+
+	http.ListenAndServe(":80", r)
+}
